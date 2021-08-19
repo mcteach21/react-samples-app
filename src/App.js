369e67f8105841a486affd0ac04c32f19aef89f2
MC-Teacher <mc.teach.21@gmail.com>
2021-08-19 09:18:17 +0200

    App update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 
+import Box from '@material-ui/core/Box';
+import Grid from '@material-ui/core/Grid';
 import { Button } from '@material-ui/core';
 
 function App() {
@@ -10,20 +12,69 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
 
         <div>
-          <Button color="primary">Tic Toe</Button>
-          <Button color="primary">Memory</Button>
+          <Button variant="outlined" color="primary" href="tictoe">Tic Toe</Button>
+          <Button variant="outlined" color="secondary" href="memory">Memory</Button>
+          <Button variant="outlined" color="primary" href="game">
+            Link
+          </Button>
         </div>
       </header>
 
       <div className="App-main">
          
-          Lorem ipsum dolor
+         <Grid container spacing={1}>
+            <Grid item xs={12} sm={4}>
+              <Box bgcolor="primary.main" color="primary.contrastText" p={2}>
+                primary.main
+              </Box>
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Box bgcolor="secondary.main" color="secondary.contrastText" p={2}>
+                secondary.main
+              </Box>
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Box bgcolor="error.main" color="error.contrastText" p={2}>
+                error.main
+              </Box>
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Box bgcolor="warning.main" color="warning.contrastText" p={2}>
+                warning.main
+              </Box>
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Box bgcolor="info.main" color="info.contrastText" p={2}>
+                info.main
+              </Box>
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Box bgcolor="success.main" color="success.contrastText" p={2}>
+                success.main
+              </Box>
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Box bgcolor="text.primary" color="background.paper" p={2}>
+                text.primary
+              </Box>
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Box bgcolor="text.secondary" color="background.paper" p={2}>
+                text.secondary
+              </Box>
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Box bgcolor="text.disabled" color="background.paper" p={2}>
+                text.disabled
+              </Box>
+            </Grid>
+          </Grid>
 
 
       </div>
 
       <footer className="App-footer">
-        <div class="footer-copyright text-center py-3">
+        <div className="footer-copyright text-center py-3">
           © 2021 Copyright <a href="https://github.com/mcteach21">mc.tech21</a>
         </div>
       </footer>
